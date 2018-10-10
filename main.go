20b10cd4f5ce6e08084cef583ff353b4c5eb7a5d
Frederiko Costa <frederiko.costa@workday.com>
2018-10-10 14:19:17 -0700

    add more information
    Change-Id: Id51a300826044a104ae969a1fe9c146cde26058a

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", helloHandler)
-	log.Println("call me on localhost:8080")
+	log.Println("hit me on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	host, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Could not get hostname", 500)
+		return
+	}
+
+	ip, err := net.LookupIP(host)
+	if err != nil {
+		http.Error(w, "Could not get ipaddress", 500)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	log.Printf("%s request from %s", r.Method, r.RemoteAddr)
-	io.WriteString(w, "Hello Pimeo!\n")
+	fmt.Fprintf(w, "You've hit %s (%s) from %s.\n", host, ip[0], r.RemoteAddr)
 }
